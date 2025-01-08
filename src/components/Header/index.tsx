effb5ad16d6b58d839c438c66ea58466c7c28506
Leonardo <leonardosilvasantosss22@gmail.com>
2025-01-08 20:52:47 -0300

    Feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
 import { Container, Logo } from "./styles";
 
+import { PiGift } from "react-icons/pi";
+
 export function Header() {
   return(
     <Container>
       <Logo>
         <h1>Amigo Secretinho</h1>
-        
+        <PiGift />
       </Logo>
 
       <span>Nunca foi tão fácil fazer um amigo secreto</span>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  display: grid;
+  justify-content: center;
+  justify-items: center;
 
+  background-color: ${({theme}) => theme.COLORS.THEME_400};
+  padding: 1rem;
+
+  font-family: "Inria Sans", serif;
+
+  > span {
+    font-size: 1.6rem;
+    color: ${({theme}) => theme.COLORS.TINTS.BLACK};
+  }
 `;
 
 export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 
+  padding: 1rem;
+
+  > h1 {
+    margin: 0;
+
+    font-size: 3rem;
+    font-weight: 700;
+    color: ${({theme}) => theme.COLORS.THEME_900};
+  }
+
+  > svg {
+    width: 4rem;
+    height: 4rem;
+    color: ${({theme}) => theme.COLORS.THEME_900};
+  }
 `;
