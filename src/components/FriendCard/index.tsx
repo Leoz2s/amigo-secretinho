97451e21f7fa648c1ad421e0c8520f34cad912ce
Leonardo <leonardosilvasantosss22@gmail.com>
2025-01-09 22:21:54 -0300

    Feat: FriendCard component

--- src/components/FriendCard/index.tsx
+++ src/components/FriendCard/index.tsx
@@ -1,9 +1,15 @@
 import { Container } from "./styles";
+import { PiPencilSimple } from "react-icons/pi";
 
-export function FriendCard() {
+type FriendCardProps = {
+  friendName: string
+}
+
+export function FriendCard({friendName}: FriendCardProps) {
   return(
     <Container>
-      
+      {friendName}
+      <PiPencilSimple />
     </Container>
   );
 };
--- src/components/FriendCard/styles.ts
+++ src/components/FriendCard/styles.ts
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
 
+  margin-bottom: 1rem;
+
+  width: 26.4rem;
+  padding: 1rem;
+  background-color: ${({theme}) => theme.COLORS.THEME_600};
+  border-radius: 99px;
+
+  color: ${({theme}) => theme.COLORS.THEME_100};
+  font-family: "Inria Sans", serif;
+  font-size: 2rem;
+
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: ${({theme}) => theme.COLORS.THEME_800};
+  }
+
+  > svg {
+    width: 2rem;
+    height: 2rem;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
