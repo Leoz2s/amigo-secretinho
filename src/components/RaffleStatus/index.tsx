78260b8e925e4d111ef47b53f989c64d51fc7cd5
Leonardo <leonardosilvasantosss22@gmail.com>
2025-01-10 22:33:27 -0300

    Feat: RaffleStatus component

--- src/components/RaffleStatus/index.tsx
+++ src/components/RaffleStatus/index.tsx
@@ -1,9 +1,42 @@
 import { Container } from "./styles";
+import { PiCircleNotch, PiCheckCircle, PiXCircle } from "react-icons/pi";
 
-export function RaffleStatus() {
+const statusOptions = ["waiting", "successful", "failed"];
+
+type RaffleStatusProps = {
+  status: string | "none" | "waiting" | "successful" | "failed";
+};
+
+export function RaffleStatus({status}: RaffleStatusProps) {
   return(
-    <Container>
-      
+    <Container className={status} >
+      <p className="upperText">
+        { 
+          status === statusOptions[0] && "Estamos sorteando os amigos..." 
+          ||
+          status === statusOptions[1] && "Vocês receberão a mensagem em breve."
+          ||
+          status === statusOptions[2] && "Algo deu errado..."
+        }
+      </p>
+
+      { 
+        status === statusOptions[0] && <PiCircleNotch />
+        ||
+        status === statusOptions[1] && <PiCheckCircle /> 
+        ||
+        status === statusOptions[2] && <PiXCircle />
+      }
+
+      <p className="lowerText" >
+        { 
+          status === statusOptions[0] && "" 
+          ||
+          status === statusOptions[1] && "Bom amigo secreto!"
+          ||
+          status === statusOptions[2] && "Tente novamente mais tarde."
+        }
+      </p>
     </Container>
   );
 };
--- src/components/RaffleStatus/styles.ts
+++ src/components/RaffleStatus/styles.ts
@@ -1,5 +1,53 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+export const Container = styled.div`
+  display: grid;
+  justify-items: center;
+  align-items: center;
 
+  text-align: center;
+
+  width: 31.3rem;
+  height: 24.6rem;
+  padding: 1rem;
+  border-radius: 30px;
+  background-color: ${({theme}) => theme.COLORS.THEME_800};
+
+  &.none {
+    display: none;
+  }
+
+  > p {
+    font-family: "Inria Sans", serif;
+    font-size: 2.4rem;
+    color: ${({theme}) => theme.COLORS.THEME_100};
+  }
+  &.successful > p.upperText {
+    max-width: 30rem;
+  }
+
+  > svg {
+    width: 10rem;
+    height: 10rem;
+  }
+  &.waiting > svg {
+    color: ${({theme}) => theme.COLORS.THEME_100};
+    animation: rotateClockwise 1.2s linear infinite;
+  }
+  &.successful > svg {
+    color: ${({theme}) => theme.COLORS.TINTS.GREEN};
+  } 
+  &.failed > svg {
+    color: ${({theme}) => theme.COLORS.TINTS.RED};
+  }
+
+  @keyframes rotateClockwise {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @media (min-width: 425px) {
+    width: 41.3rem;
+  }
 `;
