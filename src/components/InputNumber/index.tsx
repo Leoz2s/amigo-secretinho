0cc5820d1a2e62a2a683032b7543018e44d6a056
Leonardo <leonardosilvasantosss22@gmail.com>
2025-01-15 23:57:49 -0300

    Feat: InputNumber component

--- src/components/InputNumber/index.tsx
+++ src/components/InputNumber/index.tsx
@@ -1,9 +1,61 @@
-import { Container } from "./styles";
+import { useEffect, useRef, useState } from "react";
+import { Container, NumberGroup } from "./styles";
 
-export function InputNumber() {
+type InputNumberProps = {
+  label: string;
+  placeholder: string;
+};
+
+export function InputNumber({label, placeholder}: InputNumberProps) {
+  const inputRef = useRef(null);
+  const [formattedNumber, setFormattedNumber] = useState<string[]>([""]);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function numberFormatter(completeNumber: string) {
+    const country = "+55";
+    const ddd = completeNumber.slice(3, 5);
+    const number = completeNumber.slice(5);
+    setFormattedNumber([country, ddd, number]);
+  };
+
+  function handleInputClick() {
+    if(inputRef.current) {
+      inputRef.current.select();
+    };
+  };
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if(e.target && e.target.value.length <= 11) {
+      setInputValue(e.target.value);
+      numberFormatter(`+55${e.target.value}`)
+    };
+  };
+
+  useEffect(() => {
+    numberFormatter(placeholder);
+  }, []);
+
   return(
     <Container>
-      
+      <p>{label}</p>
+
+      <input ref={inputRef} type="number"
+        placeholder={placeholder}
+        onChange={e => handleInputChange(e)}
+        value={inputValue} maxLength={11}
+      />
+
+      <NumberGroup className={inputValue == "" ? "placeholder-shown" : ""} onClick={handleInputClick}>
+        <div className="number-segment country-number">
+          {formattedNumber[0]}
+        </div>
+        <div className="number-segment ddd-number">
+          {formattedNumber[1]}
+        </div>
+        <div className="number-segment friend-number">
+          {formattedNumber[2]}
+        </div>
+      </NumberGroup>
     </Container>
   );
 };
--- src/components/InputNumber/styles.ts
+++ src/components/InputNumber/styles.ts
@@ -1,5 +1,82 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+export const Container = styled.div`
+  position: relative;
 
+  > p {
+    font-family: "Inria Sans", serif;
+    color: ${({theme}) => theme.COLORS.TINTS.WHITE};
+    font-size: 2rem;
+
+    margin-bottom: .8rem;
+  }
+
+  > input {
+    width: 29.6rem;
+    padding: 1.4rem 2.1rem;
+    background: none;
+    border-radius: 99px;
+
+    z-index: 1;
+
+    font-family: "Inria Sans", serif;
+    font-size: 2rem;
+    font-weight: 700;
+    color: ${({theme}) => theme.COLORS.THEME_950};
+    &:placeholder-shown {
+      color: ${({theme}) => theme.COLORS.THEME_600};
+    }
+
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+`;
+
+export const NumberGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  /* position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 0; */
+  
+  &.placeholder-shown .number-segment {
+    color: ${({theme}) => theme.COLORS.THEME_600};
+  }
+
+  > .number-segment {
+    width: 4.7rem;
+    height: 4.7rem;
+    padding-block: 1rem;
+    
+    background-color: ${({theme}) => theme.COLORS.THEME_200};
+    border-radius: 99px;
+    
+    font-family: "Inria Sans", serif;
+    font-size: 2rem;
+    font-weight: 700;
+    color: ${({theme}) => theme.COLORS.THEME_950};
+
+    cursor: text;
+
+    &.country-number {
+      text-align: center;
+    }
+    &.ddd-number{
+      text-align: center;
+    }
+    &.friend-number {
+      width: 17.8rem;
+      text-align: center;
+    }
+  }
 `;
