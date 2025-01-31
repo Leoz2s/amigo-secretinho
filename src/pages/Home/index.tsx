c0f5986db60b42854d146cfd0b3a5dca44b33484
Leonardo <leonardosilvasantosss22@gmail.com>
2025-01-31 19:44:11 -0300

    Feat: Raffling secret friends

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Container, Main, FriendsBox, RaffleElement } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { FriendCard } from '../../components/FriendCard';
-import { RaffleStatus, RaffleStatusProps } from '../../components/RaffleStatus';
+import { RaffleStatus,  RaffleStatusProps } from '../../components/RaffleStatus';
 
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -10,7 +10,7 @@ import { useFriends, FriendObjectProps } from '../../hooks/friends';
 
 export function Home() {
   const {indexFriends} = useFriends();
-  const [friends, setFriends] = useState<[string?, string?, string?]>([]);
+  const [friends, setFriends] = useState<FriendObjectProps[]>([]);
   const [raffleState, setRaffleState] = useState<string>("none");
   const navigate =  useNavigate();
 
@@ -21,8 +21,39 @@ export function Home() {
   function handleRaffle() {
     if(raffleState == "none") {
       setRaffleState("waiting");
-      setRaffleState("successful");
-      setRaffleState("failed");
+      
+      const friends = indexFriends();
+      const raffleStructure = friends.map((friend: FriendObjectProps) => {return {id: friend.id, secretFriendID: 0}});
+      let alreadyRaffled: number[] = [];
+      let remakeRaffleCounter = 0;
+  
+      for (let index = 0; index < friends.length; index++) {
+        const friend = friends[index];
+        const randomIndex = Math.floor(Math.random() * friends.length);
+        const secretFriend = friends[randomIndex];
+
+        if(friend.id !== secretFriend.id && false == alreadyRaffled.includes(secretFriend.id)) {
+          raffleStructure[index].secretFriendID = secretFriend.id;
+          alreadyRaffled = [...alreadyRaffled, secretFriend.id];
+        }else { 
+          if(remakeRaffleCounter > 10) {
+            alreadyRaffled = [];
+            remakeRaffleCounter = 0;
+            index = -1;
+          }else {
+            index--
+            remakeRaffleCounter++
+          };
+        };
+      };
+
+      console.log(raffleStructure, "-", alreadyRaffled)
+
+      try {
+        setRaffleState("successful");
+      } catch (error) {
+        setRaffleState("failed");
+      };
     }else {
       setRaffleState("none");
     };
